48887da4435eef503f8c716c483f772fd6097446
Hangman7589 <hiero7589@gmail.com>
2014-04-26 15:16:34 +0200

    Ausgabe der Daten
    Erste Ausgabe unterdrückt, dafür ein zweite hinzugefügt

--- wetterprogramm.m
+++ wetterprogramm.m
@@ -1,1 +1,3 @@
-wetterdaten= urlread('http://api.met.no/weatherapi/locationforecast/1.8/?lat=53.143889;lon=8.213889')
+wetterdaten= urlread('http://api.met.no/weatherapi/locationforecast/1.8/?lat=53.143889;lon=8.213889');
+
+wetterdaten

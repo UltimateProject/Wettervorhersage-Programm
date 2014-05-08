88bcdfa20bd1f4c996c8d1be7d83ee97946c195a
Hangman7589 <hiero7589@gmail.com>
2014-05-08 19:37:08 +0200

    änderungen

--- windtoday.m
+++
@@ -1,7 +0,0 @@
-function [  ] = windtoday( input_args )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- windweek.m
+++
@@ -1,7 +0,0 @@
-function [  ] = windweek( input_args )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-

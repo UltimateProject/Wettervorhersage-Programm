f865800ab45e4c05c36f39b5c80638b3f8015e56
Fettnase <joseffa.baumann@jade-hs.de>
2014-05-08 09:30:31 +0200

    Weitere Visualisierungen

--- testen.m
+++ testen.m
@@ -1,129 +1,54 @@
 
-% thermometer1=imread(thermometer1);
-% thermometer1 = importdata('thermometer1.png'); 
-% thermometer2 = importdata('thermometer5_10.png'); 
-% thermometer3 = importdata('thermometer10_15.png'); 
-% thermometer4 = importdata('thermometer15_20.png'); 
-% thermometer5 = importdata('thermometer20_25.png'); 
-% thermometer6 = importdata('thermometer25_30.png'); 
-% thermometer7 = importdata('thermometer0_5.png'); 
-% sonne = importdata('sonne.jpg');
-% heiterwolkig = importdata('sonnewolken.jpg');
-% wolken = importdata('wolken.jpg');
-
-
-
-t=5;
-
+% Erstellen einer Figure
 f = figure;
 set(f, 'Units', 'normalized', 'Position', [0.0, 0.05, 1, 0.88],'Color','w');
-
+%Anhalten der Figure
 hold on
 
-% Bilder einfügen: 
-% axis off
-% axes ('Position', [1 1 1 1])
-% if t==2
-% image (bla)
-% end
-% if t==5
-% image(bla2)
-% end 
-% axis image
-% axis off
-
+% Variable erstellen für uicontrol, für Textausgabe
 datum1=uicontrol('style','text');
 datum2=uicontrol('style','text');
-datum3=uicontrol('style','text');
-datum4=uicontrol('style','text');
-datum5=uicontrol('style','text');
 
-set(datum1,'units','normalized','position', [0.18 0.95 0.1 0.05],'FontSize',21, 'string','Aktuell');
-set(datum2,'units','normalized','position', [0.62 0.95 0.2 0.05],'FontSize',21, 'string','Tagesverlauf');
+% Schriftzüge erstellen mit 'Aktuell' & 'Tagesverlauf'
+set(datum1,'units','normalized','position', [0.18 0.95 0.1 0.05],...
+    'FontSize',21, 'string','Aktuell');
+set(datum2,'units','normalized','position', [0.62 0.95 0.2 0.05],...
+    'FontSize',21, 'string','Tagesverlauf');
 
 
-morgens1=uicontrol('style','text');
+% Variable erstellen für uicontrol, für Textausgabe
 morgens2=uicontrol('style','text');
-morgens3=uicontrol('style','text');
-morgens4=uicontrol('style','text');
-morgens5=uicontrol('style','text');
 
-% set(morgens1,'units','normalized','position', [0.02 0.8 0.1 0.05],'FontSize',16, 'string','Temperatur');
-set(morgens2,'units','normalized','position', [0.4 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
+% Schriftzug erstellen mit 'Temperatur'
+set(morgens2,'units','normalized','position', ...
+    [0.4 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
 
 
-mittags1=uicontrol('style','text');
+% Variable erstellen für uicontrol, für Textausgabe
 mittags2=uicontrol('style','text');
-mittags3=uicontrol('style','text');
-mittags4=uicontrol('style','text');
-mittags5=uicontrol('style','text');
 
-% set(mittags1,'units','normalized','position', [0.02 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-set(mittags2,'units','normalized','position', [0.4 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
+% Schriftzug erstellen mit 'Bewölkung'
+set(mittags2,'units','normalized','position', ...
+    [0.4 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
 
 
-
-abends1=uicontrol('style','text');
+% Variable erstellen für uicontrol, für Textausgabe
 abends2=uicontrol('style','text');
-abends3=uicontrol('style','text');
-abends4=uicontrol('style','text');
-abends5=uicontrol('style','text');
 
-% set(abends1,'units','normalized','position', [0.02 0.3 0.1 0.05],'FontSize',10, 'string','Windrichtung und Windgeschwindigkeit');
-set(abends2,'units','normalized','position', [0.4 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
+% Schriftzug erstellen mit 'Wind'
+set(abends2,'units','normalized','position', ...
+    [0.4 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
 
-
+% Achsen ausschalten 
  axis off
-% axes('Position', [0.1 0.6 0.05 0.1])
-% if t==2
-% image(thermometer7)
-% end
-% if t==5
-% image(thermometer6)
-% end
-% axis image
-% axis off
-% 
-% axes('Position', [0.5 0.3 0.1 0.2])
-% image(thermometer3)
-% axis image
-% axis off
 
-% Push Botton für Wochenübersicht
-% hold on 
- push = uicontrol ('Style', 'pushbutton', 'Position', [0.88 0.9 0.1 0.1],....
-                   'Units', 'normalized', 'String', '5-Tage Vorschau',....
+ 
+ % Generieren eines Push-Buttons zur Auswahl auf die 5-Tage Vorschau
+ push = uicontrol ('Style', 'pushbutton','Units', 'normalized',.... 
+                   'Position', [0.8 0.5 0.1 0.1],....
+                   'String', '5-Tage Vorschau',....
                    'Callback', 'wochenuebersicht', 'Visible', 'on');
 
-
-% P=cell(5,5);
-% figure(2)
-% P{1,1}=imread('thermometer1.png');
-% P{1,3}=imread('thermometer5_10.png');
-% P{4,3}=imread('thermometer20_25.png');
-% vektor = [0,1,2,1,2,0,1,1]; 
-% image([P{vektor+1}])
-% 
-% axis off
-% % set(gca,'Position',[0 0 1 1]) 
-% 
-% 
-% 
-% 
-% figure(1)
-% hold on
-% subplot(1,5,1);
-% image(thermometer1);
-% axis off
-% subplot(1,5,2);
-% image(thermometer4);
-% axis off
-% subplot(1,5,3);
-% image(thermometer2);
-% axis off
-% subplot(1,5,4);
-% image(thermometer5);
-% axis off
-% subplot(1,5,5);
-% image(thermometer3);
-% axis off
+% Einbinden der einzelnen Funktionen für die aktuellen Tage
+temptoday_out(Tag_aktuell)
+cloudtoday( wolken_dichte_aktuell )
--- wochenuebersicht.m
+++ wochenuebersicht.m
@@ -20,50 +20,10 @@ set(datum3,'units','normalized','position', [0.45 0.92 0.1 0.05],'FontSize',21, 
 set(datum4,'units','normalized','position', [0.65 0.92 0.1 0.05],'FontSize',21, 'string','Tag4');
 set(datum5,'units','normalized','position', [0.85 0.92 0.1 0.05],'FontSize',21, 'string','Tag5');
 
-% Einlesen der Werte für 'Morgens'
-morgens1=uicontrol('style','text');
-morgens2=uicontrol('style','text');
-morgens3=uicontrol('style','text');
-morgens4=uicontrol('style','text');
-morgens5=uicontrol('style','text');
-
-% Aufteilung der Werte auf entsprechende Tage
-set(morgens1,'units','normalized','position', [0.05 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
-set(morgens2,'units','normalized','position', [0.25 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
-set(morgens3,'units','normalized','position', [0.45 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
-set(morgens4,'units','normalized','position', [0.65 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
-set(morgens5,'units','normalized','position', [0.85 0.85 0.1 0.05],'FontSize',16, 'string','Temperatur');
-
-% Einlesen der Werte für 'Mittags'
-mittags1=uicontrol('style','text');
-mittags2=uicontrol('style','text');
-mittags3=uicontrol('style','text');
-mittags4=uicontrol('style','text');
-mittags5=uicontrol('style','text');
-
-% Aufteilung der Werte auf entsprechende Tage
-set(mittags1,'units','normalized','position', [0.05 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-set(mittags2,'units','normalized','position', [0.25 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-set(mittags3,'units','normalized','position', [0.45 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-set(mittags4,'units','normalized','position', [0.65 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-set(mittags5,'units','normalized','position', [0.85 0.55 0.1 0.05],'FontSize',16, 'string','Bewölkung');
-
-% Einlesen der Werte für 'Abends'
-abends1=uicontrol('style','text');
-abends2=uicontrol('style','text');
-abends3=uicontrol('style','text');
-abends4=uicontrol('style','text');
-abends5=uicontrol('style','text');
-
-set(abends1,'units','normalized','position', [0.05 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
-set(abends2,'units','normalized','position', [0.25 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
-set(abends3,'units','normalized','position', [0.45 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
-set(abends4,'units','normalized','position', [0.65 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
-set(abends5,'units','normalized','position', [0.85 0.25 0.1 0.05],'FontSize',16, 'string','Wind');
-
 
 % Ausblenden der Axen
 axis off
 
-
-
+% Ausführen der Funktion für die Wochenübersicht der Temperaturen
+tempweek_out( Tag_aktuell, Tage_rest )
+cloudweek(wolken_dichte_aktuell, wolken_dichte_Tage_rest)
